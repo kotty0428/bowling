3e44b5f977753ba517f7cf11b022f2df389d116d
EC2 Default User <ec2-user@ip-172-31-17-100.us-east-2.compute.internal>
2021-09-20 00:56:07 +0000

    テストコードの追加

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -1,17 +1,27 @@
 require "bowling"
 
-describe "ボウリングのスコアの合計" do
+describe "ボウリングのスコア計算" do
     describe "全体の合計" do
-        context "全ての投球がガターだった場合" do
-            it "０になること" do
+        context "全ての投球がガーターだった場合" do
+            it "0になること" do
                 @game = Bowling.new
                 
                 20.times do
-                    @game.add_score(0)
+                    @game.add_scorre(0)
                 end
+                
                 expect(@game.total_score).to eq 0
             end
         end
+        
+        context "20になること" do
+            @game = Bowling.new
+            
+            20.times do
+                @game.add_scorre(1)
+            end
+            
+            expect(@game.total_score).to eq 20
+        end
     end
 end
-    
